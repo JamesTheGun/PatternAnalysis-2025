28933288078b2a89d030f9266007d3233793feb5
JamesTheGun <jamedbrunton@gmail.com>
2025-10-28 13:17:11 +1100

    glashing out data loading methods...

--- recognition/gnn-facebook-facebookpages-s4749422-James-Brunton/dataset.py
+++ recognition/gnn-facebook-facebookpages-s4749422-James-Brunton/dataset.py
@@ -1,10 +1,25 @@
 import pandas as pd
+import os
 
-def load_edges():
-    pass
 
-def load_targets():
-    pass
+def _get_data_directory():
+    return os.path.join(
+        "recognition",
+        "gnn-gacebook-facebookpages-s4749422-James-Brunton",
+        "data",
+        "facebook_large",
+    )
 
-def load_features():
-    pass
+
+def _load_edges():
+    return pd.read_csv(os.path.join(_get_data_directory(), "musae_facebook_edges.csv"))
+
+
+def _load_targets():
+    return pd.read_csv(os.path.join(_get_data_directory(), "musae_facebook_target.csv"))
+
+
+def _load_features():
+    return pd.read_json(
+        os.path.join(_get_data_directory(), "musae_facebook_features.json")
+    )
