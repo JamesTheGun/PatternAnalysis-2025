ad6725f577f593f9b00e3ea997e1d98568eecccc
JamesTheGun <jamedbrunton@gmail.com>
2025-10-30 18:32:26 +1100

    Added sage... it works very poorly.

--- recognition/gnn_facebookpages_s4749422_James_Brunton/modules.py
+++ recognition/gnn_facebookpages_s4749422_James_Brunton/modules.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, SAGEConv
 
 
 class GraphConvolutionalNetwork(nn.Module):
-    def __init__(self, in_dim: int, hidden: int, out_dim: int, p: float = 0.5):
+    def __init__(self, in_dim: int, hidden: int, out_dim: int, p: float = 0.6):
         super().__init__()
         # does all the graph maths for us...
         # we make to blocks
@@ -16,6 +16,26 @@ class GraphConvolutionalNetwork(nn.Module):
         self.drop = nn.Dropout(p)
 
     # implement forward...
+    def forward(self, x, edge_index):
+        # x with shape: [num_nodes, in_dim]
+        # edge_index are the actual connections...
+        # relu to find non-linear patterns
+        x = self.conv1(x, edge_index).relu()
+        x = self.drop(x)
+        x = self.conv2(x, edge_index).relu()
+        x = self.drop(x)
+        x = self.conv3(x, edge_index)
+        return x
+
+
+class GraphSAGE(nn.Module):
+    def __init__(self, in_dim, hidden, out_dim, p=0.5):
+        super().__init__()
+        self.conv1 = SAGEConv(in_dim, hidden, normalize=True)
+        self.conv2 = SAGEConv(hidden, hidden, normalize=True)
+        self.conv3 = SAGEConv(hidden, out_dim, normalize=True)
+        self.drop = nn.Dropout(p)
+
     def forward(self, x, edge_index):
         # x with shape: [num_nodes, in_dim]
         # edge_index are the actual connections...
